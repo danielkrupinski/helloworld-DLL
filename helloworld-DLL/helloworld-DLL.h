67057dbeaa66caa5cd9c6ecbb3dd80e76250b52c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-28 18:56:39 +0200

    Update preprocessor definitions in helloworld-DLL.h

--- helloworld-DLL/helloworld-DLL.h
+++ helloworld-DLL/helloworld-DLL.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#ifdef helloworld-DLL_EXPORTS
-#define helloworld-DLL_API __declspec(dllexport)
+#ifdef HELLOWORLDDLL_EXPORTS
+#define HELLOWORLDDLL_API __declspec(dllexport)
 #else
-#define helloworld-DLL_API __declspec(dllimport)
+#define HELLOWORLDDLL_API __declspec(dllimport)
 #endif
 
-extern "C" helloworld-DLL_API void hello();
+extern "C" HELLOWORLDDLL_API void hello();
